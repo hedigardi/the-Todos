ac60a94c165c00e08c89005f7d0ca8c485f7553b
Hedi Gardi <hedigardi@hotmail.com>
2024-03-13 09:00:11 +0100

    changed fetch to axios

--- src/services/AppService.js
+++ src/services/AppService.js
@@ -1,15 +1,9 @@
+import axios from 'axios';
+
 const fetchTodos = async () => {
   try {
-    const response = await fetch('https://random-todos.azurewebsites.net/todos?apikey=$2a$10$T6Xd4gMJLTrNeo58vWncdOM/EltSOicKlLMyZCzyJXURlicV6fuoy&amount=5&randomdone=false', {
-      headers: {
-        'Method': 'application/json'
-      }
-    });
-    if (!response.ok) {
-      throw new Error('Failed to fetch todos');
-    }
-    const data = await response.json();
-    return data;
+    const response = await axios.get('https://random-todos.azurewebsites.net/todos?apikey=$2a$10$T6Xd4gMJLTrNeo58vWncdOM/EltSOicKlLMyZCzyJXURlicV6fuoy&amount=5&randomdone=false');
+    return response.data;
   } catch (error) {
     console.error('Error fetching todos:', error);
     throw error;
